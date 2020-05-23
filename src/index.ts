99d6b325348d7d9e27e80e4f9ee65ed4e5693c71
Kasun Vithanage <alankasun@gmail.com>
2020-05-24 00:49:48 +0530

    bit refactor

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 export * from './auth/Authentication';
 export * from './auth/AuthenticationErrorResponse';
 export * from './auth/OAuth';
+
 export * from './enums/Country';
 export * from './enums/Currency';
+
 export * from './errors/AuthenticationError';
 export * from './errors/PayUError';
+
 export * from './orders/Buyer';
 export * from './orders/Order';
 export * from './orders/OrderCreateResponse';
@@ -12,6 +15,7 @@ export * from './orders/OrderCreateStatus';
 export * from './orders/OrderStatusResponse';
 export * from './orders/Product';
 export * from './orders/Status';
+
 export * from './endpoints';
 export * from './ips';
 export * from './PayU';
