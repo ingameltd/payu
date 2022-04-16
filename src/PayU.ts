d2c8f91ff885250fc832058cc680d2a35478a0bd
Kasun Vithanage <alankasun@gmail.com>
2022-04-17 02:04:12 +0530

    refactor(): code improvements

--- src/PayU.ts
+++ src/PayU.ts
@@ -1,254 +1,300 @@
-import crypto from 'crypto';
-import Axios, { AxiosInstance } from 'axios';
-import { OAuth } from './auth/OAuth';
-import { OrderCreateResponse } from './orders/OrderCreateResponse';
-import { Order } from './orders/Order';
-import { OrderEndpoint } from './endpoints';
-import { PayUError } from './errors/PayUError';
-import { OrderStatusResponse } from './orders/OrderStatusResponse';
-import { SandboxIPs, ProductionIPs } from './ips';
+import crypto from "crypto";
+import Axios, { AxiosError, AxiosInstance } from "axios";
+import { OAuth } from "./auth/OAuth";
+import { OrderCreateResponse } from "./orders/OrderCreateResponse";
+import { Order } from "./orders/Order";
+import { OrderEndpoint } from "./endpoints";
+import { PayUError } from "./errors/PayUError";
+import { OrderStatusResponse } from "./orders/OrderStatusResponse";
+import { SandboxIPs, ProductionIPs } from "./ips";
+import axios from "axios";
 
-
-const SandboxEndpoint = "https://secure.snd.payu.com";
-const ProductionEdnpoint = "https://secure.payu.com";
+const SANDBOX_ENDPOINT = "https://secure.snd.payu.com";
+const PRODUCTION_ENDPOINT = "https://secure.payu.com";
 
 export interface PayUOptions {
-    sandbox?: boolean,
+  sandbox?: boolean;
 }
 
 export class PayU {
-    private clientId: number;
-    private clientSecret: string;
-    private merchantPosId: number;
-    private secondKey: string;
-    private baseEndpoint: string;
-    private client: AxiosInstance;
-    private options: PayUOptions;
-    private oAuth: OAuth;
-    private ips: string[];
+  private baseEndpoint: string;
+  private client: AxiosInstance;
+  private oAuth: OAuth;
+  private ips: string[];
 
-    /**
-     * Creates an instance of PayU.
-     * @param {number} clientId - client id for merchant
-     * @param {string} clientSecret - client secret from panel
-     * @param {number} merchantPosId - pos id from panel
-     * @param {string} secondKey - second key from panel
-     * @param {PayUOptions} [options={ sandbox: false }] - additional options
-     * @memberof PayU
-     */
-    constructor(
-        clientId: number,
-        clientSecret: string,
-        merchantPosId: number,
-        secondKey: string,
-        options: PayUOptions = { sandbox: false }
+  /**
+   * Creates an instance of PayU.
+   * @param {number} clientId - client id for merchant
+   * @param {string} clientSecret - client secret from panel
+   * @param {number} merchantPosId - pos id from panel
+   * @param {string} secondKey - second key from panel
+   * @param {PayUOptions} [options={ sandbox: false }] - additional options
+   * @memberof PayU
+   */
+  constructor(
+    private readonly clientId: number,
+    private readonly clientSecret: string,
+    private readonly merchantPosId: number,
+    private readonly secondKey: string,
+    private readonly options: PayUOptions = { sandbox: false }
+  ) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.merchantPosId = merchantPosId;
+    this.secondKey = secondKey;
+    this.options = options;
+
+    this.baseEndpoint = !this.options.sandbox
+      ? PRODUCTION_ENDPOINT
+      : SANDBOX_ENDPOINT;
+    this.ips = !this.options.sandbox ? ProductionIPs : SandboxIPs;
+    this.client = Axios.create({ baseURL: this.baseEndpoint });
+
+    this.oAuth = new OAuth(this.client, this.clientId, this.clientSecret);
+  }
+
+  /**
+   * Get access token
+   *
+   * @returns {Promise<string>}
+   * @throws {AuthenticationError}
+   * @memberof PayU
+   */
+  public async getAccessToken(): Promise<string> {
+    return this.oAuth.getAccessToken();
+  }
+
+  /**
+   * Create a new order
+   *
+   * @param {Order} order - order to be created
+   * @returns {Promise<OrderCreateResponse>}
+   * @throws {PayUError}
+   * @memberof PayU
+   */
+  public async createOrder(order: Order): Promise<OrderCreateResponse> {
+    const token = await this.oAuth.getAccessToken();
+    const data = {
+      ...order,
+      merchantPosId: this.merchantPosId,
+    };
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    try {
+      const response = await this.client.post(OrderEndpoint, data, {
+        headers: headers,
+        maxRedirects: 0,
+        validateStatus: (status) => {
+          return status === 302;
+        },
+      });
+
+      return <OrderCreateResponse>response.data;
+    } catch (error) {
+      const errors = error as Error | AxiosError;
+
+      if (axios.isAxiosError(errors)) {
+        const resp = <OrderStatusResponse>errors?.response?.data;
+        throw new PayUError(
+          resp.status.statusCode,
+          resp.status.code || "",
+          resp.status.codeLiteral,
+          resp.status.statusDesc
+        );
+      }
+
+      throw error;
+    }
+  }
+
+  /**
+   * Captures an order from payU making it approved
+   *
+   * @param {string} orderId
+   * @returns {Promise<OrderStatusResponse>}
+   * @throws {PayUError}
+   * @memberof PayU
+   */
+  public async captureOrder(orderId: string): Promise<OrderStatusResponse> {
+    const token = await this.oAuth.getAccessToken();
+    const data = {
+      orderId: orderId,
+      orderStatus: "COMPLETED",
+    };
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    try {
+      const response = await this.client.put(
+        `${OrderEndpoint}/${orderId}/status`,
+        data,
+        {
+          headers: headers,
+        }
+      );
+
+      return <OrderStatusResponse>response.data;
+    } catch (error) {
+      const errors = error as Error | AxiosError;
+
+      if (axios.isAxiosError(errors)) {
+        const resp = <OrderStatusResponse>errors?.response?.data;
+        throw new PayUError(
+          resp.status.statusCode,
+          resp.status.code || "",
+          resp.status.codeLiteral,
+          resp.status.statusDesc
+        );
+      }
+
+      throw error;
+    }
+  }
+
+  /**
+   * Cancels a PayU order
+   *
+   * @param {string} orderId
+   * @returns {Promise<OrderStatusResponse>}
+   * @memberof PayU
+   */
+  public async cancelOrder(orderId: string): Promise<OrderStatusResponse> {
+    const token = await this.oAuth.getAccessToken();
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    try {
+      const response = await this.client.delete(`${OrderEndpoint}/${orderId}`, {
+        headers: headers,
+      });
+
+      return <OrderStatusResponse>response.data;
+    } catch (error) {
+      const errors = error as Error | AxiosError;
+      if (axios.isAxiosError(errors)) {
+        const resp = <OrderStatusResponse>errors?.response?.data;
+        throw new PayUError(
+          resp.status.statusCode,
+          resp.status.code || "",
+          resp.status.codeLiteral,
+          resp.status.statusDesc
+        );
+      }
+
+      throw error;
+    }
+  }
+
+  /**
+   * Refunds a PayU order
+   *
+   * @param {string} orderId - payu order id
+   * @param {string} description - description for refund
+   * @returns {Promise<OrderStatusResponse>}
+   * @memberof PayU
+   */
+  public async refundOrder(
+    orderId: string,
+    description: string
+  ): Promise<OrderStatusResponse> {
+    const token = await this.oAuth.getAccessToken();
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    try {
+      const response = await this.client.post(
+        `${OrderEndpoint}/${orderId}/refund`,
+        {
+          refund: {
+            description,
+          },
+        },
+        {
+          headers: headers,
+        }
+      );
+
+      return <OrderStatusResponse>response.data;
+    } catch (error) {
+      const errors = error as Error | AxiosError;
+
+      if (axios.isAxiosError(errors)) {
+        const resp = <OrderStatusResponse>errors?.response?.data;
+        throw new PayUError(
+          resp.status.statusCode,
+          resp.status.code || "",
+          resp.status.codeLiteral,
+          resp.status.statusDesc
+        );
+      }
+
+      throw errors;
+    }
+  }
+
+  /**
+   * Convert a key=value; list to json
+   *
+   * @private
+   * @param {string} data - key value string
+   * @returns {Record<string, string>}
+   * @memberof PayU
+   */
+  private parseHeaderToJson(data: string): Record<string, string> {
+    return data.split(";").reduce((acc, curr) => {
+      if (curr) {
+        const [key, val] = curr.split("=");
+        acc[key] = val;
+      }
+      return acc;
+    }, {} as Record<string, string>);
+  }
+
+  /**
+   * Verify notification result with signature
+   *
+   * @param {string} payuHeader - header string from **OpenPayu-Signature**
+   * @param {string} jsonNotification - notification body as a string
+   * @returns {boolean}
+   * @memberof PayU
+   */
+  public verifyNotification(
+    payuHeader: string,
+    jsonNotification: string
+  ): boolean {
+    const tokens = this.parseHeaderToJson(payuHeader);
+    if (
+      !tokens["signature"] ||
+      tokens["signature"] === "" ||
+      !tokens["algorithm"] ||
+      tokens["algorithm"] === ""
     ) {
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
-        this.merchantPosId = merchantPosId;
-        this.secondKey = secondKey;
-        this.options = options
-
-        this.baseEndpoint = !this.options.sandbox ? ProductionEdnpoint : SandboxEndpoint;
-        this.ips = !this.options.sandbox ? ProductionIPs : SandboxIPs;
-        this.client = Axios.create({ baseURL: this.baseEndpoint })
-
-        this.oAuth = new OAuth(this.client, this.clientId, this.clientSecret);
+      return false;
     }
 
-    /**
-     * Get access token
-     *
-     * @returns {Promise<string>}
-     * @throws {AuthenticationError}
-     * @memberof PayU
-     */
-    public async getAccessToken (): Promise<string> {
-        return this.oAuth.getAccessToken()
-    }
+    const concatnated = jsonNotification + this.secondKey;
+    const exceptedSignature = crypto
+      .createHash("md5")
+      .update(concatnated)
+      .digest("hex");
+    const incomingSignature = tokens["signature"];
 
-    /**
-     * Create a new order
-     *
-     * @param {Order} order - order to be created
-     * @returns {Promise<OrderCreateResponse>}
-     * @throws {PayUError}
-     * @memberof PayU
-     */
-    public async createOrder (order: Order): Promise<OrderCreateResponse> {
-        const token = await this.oAuth.getAccessToken();
-        const data = {
-            ...order,
-            merchantPosId: this.merchantPosId
-        };
-        const headers = {
-            'Authorization': `Bearer ${token}`,
-        };
+    return exceptedSignature === incomingSignature;
+  }
 
-        try {
-            const response = await this.client.post(
-                OrderEndpoint,
-                data,
-                {
-                    headers: headers,
-                    maxRedirects: 0,
-                    validateStatus: (status) => {
-                        return status === 302
-                    }
-                }
-            );
-
-            return <OrderCreateResponse>response.data;
-        } catch (error) {
-            const resp = <OrderStatusResponse>error.response.data;
-            throw new PayUError(resp.status.statusCode, resp.status.code || '', resp.status.codeLiteral, resp.status.statusDesc);
-        }
-    }
-
-    /**
-     * Captures an order from payU making it approved
-     *
-     * @param {string} orderId
-     * @returns {Promise<OrderStatusResponse>}
-     * @throws {PayUError}
-     * @memberof PayU
-     */
-    public async captureOrder (orderId: string): Promise<OrderStatusResponse> {
-        const token = await this.oAuth.getAccessToken();
-        const data = {
-            orderId: orderId,
-            orderStatus: "COMPLETED"
-        };
-        const headers = {
-            'Authorization': `Bearer ${token}`,
-        };
-
-        try {
-            const response = await this.client.put(
-                `${OrderEndpoint}/${orderId}/status`,
-                data,
-                {
-                    headers: headers,
-                }
-            );
-
-            return <OrderStatusResponse>response.data;
-        } catch (error) {
-            const resp = <OrderStatusResponse>error.response.data;
-            throw new PayUError(resp.status.statusCode, resp.status.code || '', resp.status.codeLiteral, resp.status.statusDesc);
-        }
-    }
-
-    /**
-     * Cancels a PayU order
-     *
-     * @param {string} orderId
-     * @returns {Promise<OrderStatusResponse>}
-     * @memberof PayU
-     */
-    public async cancelOrder (orderId: string): Promise<OrderStatusResponse> {
-        const token = await this.oAuth.getAccessToken();
-        const headers = {
-            'Authorization': `Bearer ${token}`,
-        };
-
-        try {
-            const response = await this.client.delete(
-                `${OrderEndpoint}/${orderId}`,
-                {
-                    headers: headers,
-                }
-            );
-
-            return <OrderStatusResponse>response.data;
-        } catch (error) {
-            const resp = <OrderStatusResponse>error.response.data;
-            throw new PayUError(resp.status.statusCode, resp.status.code || '', resp.status.codeLiteral, resp.status.statusDesc);
-        }
-    }
-
-    /**
-     * Refunds a PayU order
-     *
-     * @param {string} orderId - payu order id
-     * @param {string} description - description for refund
-     * @returns {Promise<OrderStatusResponse>}
-     * @memberof PayU
-     */
-    public async refundOrder (orderId: string, description: string): Promise<OrderStatusResponse> {
-        const token = await this.oAuth.getAccessToken();
-        const headers = {
-            'Authorization': `Bearer ${token}`,
-        };
-
-        try {
-            const response = await this.client.post(
-                `${OrderEndpoint}/${orderId}/refund`,
-                {
-                    refund: {
-                        description
-                    }
-                },
-                {
-                    headers: headers
-                }
-            );
-
-            return <OrderStatusResponse>response.data;
-        } catch (error) {
-            const resp = <OrderStatusResponse>error.response.data;
-            throw new PayUError(resp.status.statusCode, resp.status.code || '', resp.status.codeLiteral, resp.status.statusDesc);
-        }
-    }
-
-    /**
-     * Convert a key=value; list to json
-     *
-     * @private
-     * @param {string} data - key value string
-     * @returns {any}
-     * @memberof PayU
-     */
-    private parseHeaderToJson (data: string): any {
-        const parts = data.split(';');
-        let obj: any = {};
-        parts.forEach(part => {
-            const tok = part.split('=');
-            obj[tok[0]] = tok[1];
-        });
-
-        return obj;
-    }
-
-    /**
-     * Verify notification result with signature
-     * 
-     * @param {string} payuHeader - header string from **OpenPayu-Signature**
-     * @param {string} jsonNotification - notification body as a string
-     * @returns {boolean}
-     * @memberof PayU
-     */
-    public verifyNotification (payuHeader: string, jsonNotification: string): boolean {
-        const tokens = this.parseHeaderToJson(payuHeader);
-        if (!tokens['signature'] || tokens['signature'] === '' || !tokens['algorithm'] || tokens['algorithm'] === '') {
-            return false;
-        }
-
-        const concatnated = jsonNotification + this.secondKey;
-        const exceptedSignature = crypto.createHash('md5').update(concatnated).digest('hex');
-        const incomingSignature = tokens['signature'];
-
-        return exceptedSignature === incomingSignature;
-    }
-
-    /**
-     * Validates the IP address with PayU servers
-     *
-     * @param {string} ip - ip address
-     * @returns {boolean}
-     * @memberof PayU
-     */
-    public isIpValid (ip: string): boolean {
-        return this.ips.includes(ip);
-    }
+  /**
+   * Validates the IP address with PayU servers
+   *
+   * @param {string} ip - ip address
+   * @returns {boolean}
+   * @memberof PayU
+   */
+  public isIpValid(ip: string): boolean {
+    return this.ips.includes(ip);
+  }
 }
--- src/auth/OAuth.ts
+++ src/auth/OAuth.ts
@@ -1,78 +1,88 @@
-import querystring from 'querystring';
-import moment from 'moment';
-import { AxiosInstance } from 'axios';
-import { AuthenticationResponse } from './Authentication';
-import { AuthorizeEndpoint } from '../endpoints';
-import { AuthenticationErrorResponse } from './AuthenticationErrorResponse';
-import { AuthenticationError } from '../errors/AuthenticationError';
+import querystring from "querystring";
+import moment from "moment";
+import axios, { AxiosInstance, AxiosError } from "axios";
+import { AuthenticationResponse } from "./Authentication";
+import { AuthorizeEndpoint } from "../endpoints";
+import { AuthenticationErrorResponse } from "./AuthenticationErrorResponse";
+import { AuthenticationError } from "../errors/AuthenticationError";
 
 export class OAuth {
-    private client: AxiosInstance;
-    private clientId: number;
-    private clientSecret: string;
+  private accessToken: string;
+  private expiry: Date;
 
-    private _accessToken: string;
-    private _expiry: Date;
+  /**
+   *Creates an instance of OAuth.
+   * @param {AxiosInstance} client - configured axios client for proper backend
+   * @param {number} clientId - client id
+   * @param {string} clientSecret - client secret
+   * @memberof OAuth
+   */
+  constructor(
+    private readonly client: AxiosInstance,
+    private readonly clientId: number,
+    private readonly clientSecret: string
+  ) {
+    // initialize the authenticater to have invalidated expiry date
+    // so it will fetch new one on first try
+    this.expiry = moment().subtract(1, "minute").toDate();
+    this.accessToken = "";
+  }
 
-    /**
-     *Creates an instance of OAuth.
-     * @param {AxiosInstance} client - configured axios client for proper backend
-     * @param {number} clientId - client id
-     * @param {string} clientSecret - client secret
-     * @memberof OAuth
-     */
-    constructor(client: AxiosInstance, clientId: number, clientSecret: string) {
-        this.client = client;
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
+  private async _fetchAccessToken(): Promise<AuthenticationResponse> {
+    const data = {
+      grant_type: "client_credentials",
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
+    };
 
-        // initialize the authenticater to have invalidated expiry date
-        // so it will fetch new one on first try
-        this._expiry = moment().subtract(1, 'minute').toDate()
-        this._accessToken = '';
+    const config = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
+    try {
+      const response = await this.client.post(
+        AuthorizeEndpoint,
+        querystring.stringify(data),
+        config
+      );
+      const auth = <AuthenticationResponse>response.data;
+      return auth;
+    } catch (error) {
+      this.accessToken = "";
+      this.expiry = moment().subtract(1, "minute").toDate();
+
+      const errors = error as Error | AxiosError;
+
+      if (axios.isAxiosError(errors)) {
+        const errResponse = <AuthenticationErrorResponse>errors?.response?.data;
+        throw new AuthenticationError(
+          errResponse.error,
+          errResponse.error_description
+        );
+      }
+
+      throw error;
+    }
+  }
+
+  /**
+   * Get access token from PayU service
+   *
+   * @returns {Promise<string>} - access token
+   * @throws {AuthenticationError}
+   * @memberof Auth
+   */
+  public async getAccessToken(): Promise<string> {
+    // valid token(valid for 99%)
+    if (moment().isBefore(this.expiry, "seconds") && this.accessToken !== "") {
+      return this.accessToken;
     }
 
-    private async _fetchAccessToken (): Promise<AuthenticationResponse> {
-        const data = {
-            grant_type: "client_credentials",
-            client_id: this.clientId,
-            client_secret: this.clientSecret,
-        }
+    const token = await this._fetchAccessToken();
+    this.accessToken = token.access_token;
+    this.expiry = moment().add(token.expires_in, "seconds").toDate();
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        }
-        try {
-            const response = await this.client.post(AuthorizeEndpoint, querystring.stringify(data), config);
-            const auth = <AuthenticationResponse>response.data;
-            return auth;
-        } catch (error) {
-            this._accessToken = '';
-            this._expiry = moment().subtract(1, 'minute').toDate()
-            const errResponse = <AuthenticationErrorResponse>error.response.data;
-            throw new AuthenticationError(errResponse.error, errResponse.error_description);
-        }
-    }
-
-    /**
-     * Get access token from PayU service
-     *
-     * @returns {Promise<string>} - access token
-     * @throws {AuthenticationError}
-     * @memberof Auth
-     */
-    public async getAccessToken (): Promise<string> {
-        // valid token(valid for 99%)
-        if (moment().isBefore(this._expiry, "seconds") && this._accessToken !== '') {
-            return this._accessToken;
-        }
-
-        const token = await this._fetchAccessToken()
-        this._accessToken = token.access_token;
-        this._expiry = moment().add(token.expires_in, "seconds").toDate();
-
-        return this._accessToken;
-    }
+    return this.accessToken;
+  }
 }
